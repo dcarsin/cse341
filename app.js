5d91b8dcad12d4570243665fb05e5e569531b05c
dcarsin <dcarsin@byui.edu>
2021-10-10 02:32:04 -0300

    week4

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
-// const mongoConnect = require('./util/database')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -17,14 +18,40 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('6162466d4ef02deddda224dd')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(process.env.PORT || 3000);
+mongoose
+  .connect(
+    'mongodb+srv://dcarsin:ETK5VmI54xFFOrT1@cluster0.vfklm.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'David',
+          email: 'dcarsin@byui.edu',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
 
-// mongoConnect((client) => {
-//     console.log(client);
-//     app.listen(3000);  
-// })
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
