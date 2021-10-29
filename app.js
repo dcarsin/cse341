bc3c43b0f0a915712343ff74e39270086c64037e
dcarsin <dcarsin@byui.edu>
2021-10-28 23:38:58 -0300

    test

--- app.js
+++ app.js
@@ -4,22 +4,47 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
-
+const MONGODB_URI = 'mongodb+srv://dcarsin:ETK5VmI54xFFOrT1@cluster0.vfklm.mongodb.net/shop?retryWrites=true&w=majority'
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById('6162466d4ef02deddda224dd')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -27,29 +52,20 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://dcarsin:ETK5VmI54xFFOrT1@cluster0.vfklm.mongodb.net/shop?retryWrites=true&w=majority'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'David',
-          email: 'dcarsin@byui.edu',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    })
-
     app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
